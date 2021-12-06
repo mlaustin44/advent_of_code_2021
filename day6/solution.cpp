12556ae8c20733f4b924c6852d97ce16680f860a
Matthew Austin <matthew@aescape.cp>
2021-12-06 09:45:34 -0600

    solved part 2 with a dynamic approach

--- day6/solution.cpp
+++ day6/solution.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// naive approach for part 1
 vector<int> getStateDay(vector<int> startState, int day) {
     vector<int> state = startState;
     for (int i=0; i<day; i++) {
@@ -19,6 +20,39 @@ vector<int> getStateDay(vector<int> startState, int day) {
         }
     }
     return state;
+}
+
+// smart approach for part 2
+long long getFishOnDay(vector<int> startState, int day) {
+    // define a vector with one element for each fish age
+    vector<long long> state(9,0);
+    //account for the original fish
+    for (int f : startState) {
+        state[f] += 1;
+    }
+
+    for (int i=0; i<day; i++) {
+        // find the number of fish that need to spawn
+        long long spawningFish = state[0];
+        // slide the rest of the fish down.  
+        //      could do this in a loop but there's only 9 operations and 2 are special cases
+        state[0] = state[1];
+        state[1] = state[2];
+        state[2] = state[3];
+        state[3] = state[4];
+        state[4] = state[5];
+        state[5] = state[6];
+        state[6] = state[7] + spawningFish;
+        state[7] = state[8];
+        state[8] = spawningFish;
+    }
+
+    // calculate the sum of fish
+    long long totalFish = 0;
+    for (long d : state) {
+        totalFish += d;
+    }
+    return totalFish;
 }
 
 int main(int argc, char *argv[]) {
@@ -36,7 +70,12 @@ int main(int argc, char *argv[]) {
     for (int i=0; i<rawIn.size(); i+=2) {
         startState.push_back(rawIn[i] - char('0'));
     }
-    int targetDay = 80;
-    vector<int> finalState = getStateDay(startState, targetDay);
-    cout << "On day: " << targetDay << " there are: " << finalState.size() << " fish!\n";
+    int targetDay;
+    if (argc == 3) {
+        targetDay = stoi(argv[2]);
+    } else{
+        targetDay = 256;
+    }
+    long long totalFish = getFishOnDay(startState, targetDay);
+    cout << "On day: " << targetDay << " there are: " << totalFish << " fish!\n";
 }
